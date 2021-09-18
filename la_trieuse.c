bb74ba0d776d69d38a99d3cae89ff874f0dced39
Ibraim A <ibraim.asn@gmail.com>
2021-09-18 09:07:31 +0200

    remove blank spaces

--- la_trieuse.c
+++ la_trieuse.c
@@ -46,16 +46,13 @@ void merge(int *arr, int l, int m, int r)
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
-
 	/* create temp arrays */
 	int L[n1], R[n2];
-
 	/* Copy data to temp arrays L[] and R[] */
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < n2; j++)
 		R[j] = arr[m + 1 + j];
-
 	/* Merge the temp arrays back into arr[l..r]*/
 	i = 0; // Initial index of first subarray
 	j = 0; // Initial index of second subarray
@@ -71,7 +68,6 @@ void merge(int *arr, int l, int m, int r)
 		}
 		k++;
 	}
-
 	/* Copy the remaining elements of L[], if there
 	are any */
 	while (i < n1) {
@@ -79,7 +75,6 @@ void merge(int *arr, int l, int m, int r)
 		i++;
 		k++;
 	}
-
 	/* Copy the remaining elements of R[], if there
 	are any */
 	while (j < n2) {
@@ -97,11 +92,9 @@ void merge_sort(int *arr, int l, int r)
 		// Same as (l+r)/2, but avoids overflow for
 		// large l and h
 		int m = l + (r - l) / 2;
-
 		// Sort first and second halves
 		merge_sort(arr, l, m);
 		merge_sort(arr, m + 1, r);
-
 		merge(arr, l, m, r);
 	}
 }
